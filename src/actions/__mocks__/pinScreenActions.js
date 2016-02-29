b575ea5874c4dc1c9c1eb33c7518dcaa053004f6
faure <hi@faure.hu>
2016-02-29 19:17:25 +0000

    Wrote test files to demonstrate test complications

---
+++ src/actions/__mocks__/pinScreenActions.js
@@ -0,0 +1,28 @@
+function sendPin(pin) {
+  return {
+    type: 'SEND_PIN',
+    pin: pin
+  }
+}
+
+function receivePin(json) {
+  return {
+    type: 'RECEIVE_PIN',
+    json: json
+  }
+}
+
+function pinError(err) {
+  return {
+    type: 'PIN_ERROR',
+    error: err
+  }
+}
+
+export function tryPin(pin) {
+  if(pin === 1234) {
+    dispatch(sendPin(pin))
+  } else {
+    dispatch(pinError({error: 'sup'}))
+  }
+}
---
+++ src/reducers/__tests__/pin-test.js
@@ -0,0 +1,13 @@
+// jest.autoMockOff()
+// let pin = require('../index').default
+// let pinAction = require('../../actions/pinScreenActions')
+//
+// describe('When in the Pin Screen', () => {
+//   it('changes screen when correct pin is inserted', () => {
+//     console.log(pinAction.tryPin(1234))
+//     expect(true).toBe(true)
+//   })
+//   it('shows an error message when pin is not valid', () => {
+//     expect(true).toBe(true)
+//   })
+// })
