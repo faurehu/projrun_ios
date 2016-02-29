7f882ff8a5261df1d7f636c6498b65b756535618
faure <hi@faure.hu>
2016-02-29 18:34:07 +0000

    Styles

--- src/components/PinScreen.js
+++ src/components/PinScreen.js
@@ -2,7 +2,8 @@ import React, {
   Component,
   View,
   TextInput,
-  Text
+  Text,
+  Image
 } from 'react-native'
 
 export default class PinScreen extends Component {
@@ -23,7 +24,7 @@ export default class PinScreen extends Component {
     let text = !isFetching ? 'Please enter your PIN' : `Attempting with PIN ${pin}`
     if(error) {
       if(error == 'PIN_INVALID') {
-        text = 'Your PIN was not valid, please try again'
+        text = 'Your PIN was not valid'
       } else {
         text = 'There was an internal problem'
       }
@@ -34,17 +35,25 @@ export default class PinScreen extends Component {
   render() {
     let { onChangeTextHandler, textLogic } = this
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>
-          {textLogic()}
-        </Text>
-        <TextInput
-          autoFocus={true}
-          keyboardType='numeric'
-          onChangeText={onChangeTextHandler}
-          maxLength={4}
-          ref={component => this._textInput = component}
-          style={{height: 40, fontSize: 40, textAlign: 'center'}}/>
+      <View style={{flex: 1, backgroundColor: 'rgb(0, 121, 197)'}}>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Image source={require('../images/NHS.png')}/>
+        </View>
+        <View backgroundColor='white' borderTopColor='82A9D0'
+          borderTopWidth={5} style={{alignItems: 'center', justifyContent: 'flex-start'}}>
+          <Text style={{color: this.props.error ? 'red' : '5A7684', height: 40,
+            fontSize: 20, textAlign: 'center', marginTop: 20}}>
+            {textLogic()}
+          </Text>
+          <TextInput
+            autoFocus={true}
+            keyboardType='numeric'
+            onChangeText={onChangeTextHandler}
+            maxLength={4}
+            ref={component => this._textInput = component}
+            style={{height: 40, fontSize: 40, textAlign: 'center', color: '5A7684'}}/>
+        </View>
+        <View backgroundColor='white' style={{height: 250}} />
       </View>
     )
   }
